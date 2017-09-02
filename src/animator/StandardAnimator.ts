b1a86bd4dcaf1445dea1b036abc08e9733120e89
James Ehly <jehly@sycorr.com>
2017-09-02 13:10:38 -0500

    feat[animation]: add transition functions for animation effects

--- src/animator/StandardAnimator.ts
+++ src/animator/StandardAnimator.ts
@@ -1,5 +1,13 @@
 import { IAnimator } from './Animators';
 
+/**
+ * Outkit's Animator Class
+ * 
+ * @export
+ * @package Outkit
+ * @class StandardAnimator
+ * @implements {IAnimator}
+ */
 export default class StandardAnimator implements IAnimator {
 
     public start: number;
@@ -7,11 +15,13 @@ export default class StandardAnimator implements IAnimator {
     private _step: Function;
     private _interval: number;
     private _rate: number;
+    private _transition: Function;
 
     public constructor() {
         this._duration = 200;
         this._step = () => { };
         this._rate = 16;
+        this._transition = this.linear;
     }
 
     setStep(step: Function): this {
@@ -29,29 +39,120 @@ export default class StandardAnimator implements IAnimator {
         return this;
     }
 
+    setTransition(transition: AnimatorTransition) {
+        switch (transition) {
+            case AnimatorTransition.EaseIn:
+                this._transition = this.easeIn;
+                break;
+            case AnimatorTransition.EaseOut:
+                this._transition = this.easeOut;
+                break;
+            case AnimatorTransition.EaseInOut:
+                this._transition = this.easeInOut;
+                break;
+            case AnimatorTransition.PullIn:
+                this._transition = this.pullIn;
+                break;
+            case AnimatorTransition.PushOut:
+                this._transition = this.pushOut;
+                break;
+                case AnimatorTransition.PushPull:
+                this._transition = this.pushPull;
+                break;
+            default:
+                this._transition = this.linear;
+                break;
+        }
+    }
+
     /**
      * Animate calling a step function over duration. Step is called with delta
      * time so that animations complete within the duration.
      * @param start date
      */
-    animate(start?: number, ...args : any[]): Promise<boolean> {
+    animate(start?: number, ...args: any[]): Promise<boolean> {
         return new Promise((resolve) => {
-            this._interval = window.setInterval(() => {
-                let deltaTime = Date.now();
-                let timePassed = deltaTime - start;
-                let progress = timePassed / this._duration;
+            if (typeof window['requestAnimationFrame'] === 'function') {
+                let start = performance.now();
+                const rafAnimate = (time) => {
+                    let progress = (time - start) / this._duration;
+                    if (progress > 1) progress = 1;
+ 
+                    // calculate the current animation state
+                    let delta = this._transition(progress)
 
-                if (progress > 1) progress = 1
+                    this._step(delta, args);
 
-                var delta = progress;
-
-                this._step(delta, args);
-
-                if (progress == 1) {
-                    clearInterval(this._interval);
-                    resolve(true);
+                    if (progress < 1) {
+                        requestAnimationFrame(rafAnimate);
+                    } else {
+                        resolve(true);
+                    }  
                 }
-            }, this._rate)
+                requestAnimationFrame(rafAnimate);
+            } else {
+                this._interval = window.setInterval(() => {
+                    let deltaTime = Date.now();
+                    let timePassed = deltaTime - start;
+                    let progress = timePassed / this._duration;
+
+                    if (progress > 1) progress = 1
+
+                    var delta = this._transition(progress);
+
+                    this._step(delta, args);
+
+                    if (progress == 1) {
+                        clearInterval(this._interval);
+                        resolve(true);
+                    }
+                }, this._rate)
+            }
         })
     }
+
+    private linear(progress: number) {
+        return progress;
+    }
+
+    private easeIn(progress: number) {
+        return Math.pow(progress, 5);
+    }
+
+    private easeOut = this.makeEaseOut(this.easeIn);
+
+    private easeInOut = this.makeEaseInOut(this.easeIn);
+
+    private pullIn(progress: number, x: number = 2) {
+        return Math.pow(progress, 2) * ((x + 1) * progress - x)
+    }
+
+    private pushOut = this.makeEaseOut(this.pullIn);
+
+    private pushPull = this.makeEaseInOut(this.pullIn);
+
+    private makeEaseOut(timing: Function) {
+        return function (progress: number) {
+            return 1 - timing(1 - progress);
+        }
+    }
+
+    private makeEaseInOut(timing) {
+        return function (progress) {
+            if (progress < .5)
+                return timing(2 * progress) / 2;
+            else
+                return (2 - timing(2 * (1 - progress))) / 2;
+        }
+    }
+}
+
+export enum AnimatorTransition {
+    Linear,
+    EaseIn,
+    EaseOut,
+    EaseInOut,
+    PullIn,
+    PushOut,
+    PushPull
 }
