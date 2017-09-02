220ad1263518cb295ac675d41ca72bd109558086
James Ehly <jehly@sycorr.com>
2017-09-02 14:38:16 -0500

    update dist files

--- dist/outkit.js
+++ dist/outkit.js
@@ -1,4 +1,4 @@
-/*! Outkit v0.2.4 - Copyright 2017 James Ehly - MIT License */
+/*! Outkit v0.2.5 - Copyright 2017 James Ehly - MIT License */
 (function webpackUniversalModuleDefinition(root, factory) {
 	if(typeof exports === 'object' && typeof module === 'object')
 		module.exports = factory();
