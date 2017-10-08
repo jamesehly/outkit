3480eded711d3ae112f027d2d93ab0f4f6c9e1ab
James Ehly <james@ehlydesign.com>
2017-10-07 23:11:10 -0500

    fix(window): add default options and placement

--- dist/components/Window.d.ts
+++ dist/components/Window.d.ts
@@ -7,12 +7,27 @@ export interface WindowOptions {
     left?: number;
     bottom?: number;
     right?: number;
+    placement?: WindowPlacement;
+    placementMargin?: number;
+}
+export declare enum WindowPlacement {
+    TopLeft = 0,
+    Top = 1,
+    TopRight = 2,
+    Left = 3,
+    Center = 4,
+    Right = 5,
+    BottomRight = 6,
+    Bottom = 7,
+    BottomLeft = 8,
 }
 export default class Window extends Component {
     private _width;
     private _height;
     private _top;
     private _left;
+    private _placement;
+    private _placementMargin;
     private _isOpen;
     constructor(element: string, options?: WindowOptions);
     width(n: number): this;
--- src/components/Window.ts
+++ src/components/Window.ts
@@ -3,12 +3,26 @@ import { IAnimator } from 'outkit-animator';
 import { Component } from "./Component";
 
 export interface WindowOptions {
-    width?:number,
+    width?: number,
     height?: number,
     top?: number,
-    left?:number,
-    bottom?:number,
-    right?: number
+    left?: number,
+    bottom?: number,
+    right?: number,
+    placement?: WindowPlacement,
+    placementMargin?: number
+}
+
+export enum WindowPlacement {
+    TopLeft,
+    Top,
+    TopRight,
+    Left,
+    Center,
+    Right,
+    BottomRight,
+    Bottom,
+    BottomLeft
 }
 
 export default class Window extends Component {
@@ -17,17 +31,21 @@ export default class Window extends Component {
     private _height: number;
     private _top: number;
     private _left: number;
+    private _placement: WindowPlacement;
+    private _placementMargin: number;
     private _isOpen: boolean;
 
     constructor(element: string, options?: WindowOptions) {
         super(element);
 
         // Setup defaults
-        this._width = 0;
-        this._height = 0;
-        this._top = 0;
-        this._left = 0;
+        this._width = options.width || 100;
+        this._height = options.height || 100;
+        this._top = options.top || 0;
+        this._left = options.left || 0;
+        this._placement = WindowPlacement.Center
         this._isOpen = false;
+        this._placementMargin = 32;
 
         // Relay events
         this.registerEvent('on', () => { return this.on() });
@@ -74,10 +92,79 @@ export default class Window extends Component {
         state.style.position = 'absolute';
         state.style.display = 'none';
         state.style.zIndex = '9999'
-        state.style.width = `${this.getElement().parentElement.offsetWidth / 2}px`;
-        state.style.height = `${this.getElement().parentElement.offsetHeight / 2}px`;
-        state.style.left = `${this._left}px`;
-        state.style.top = `${this._top}px`;
+        state.style.width = `${this._width}px`;
+        state.style.height = `${this.height}px`;
+        state.style.opacity = '0';
+        let left = '0';
+        let top = '0';
+        let parentWidth = this.getElement().parentElement.offsetWidth;
+        let parentHeight = this.getElement().parentElement.offsetHeight;
+        let margin = this._placementMargin;
+        let right = parentWidth - this._width - margin;
+        let center = parentWidth / 2 - this._width / 2;
+        let middle = parentHeight / 2 - this._height / 2;
+        let bottom = parentHeight - this._height - margin;
+        if (this._placement) {
+            switch (+this._placement) {
+                case WindowPlacement.TopLeft:
+                    left = `${margin}px`;
+                    top = `${margin}px`;
+                    break;
+
+                case WindowPlacement.Top:
+                    left = `${center}px`;
+                    top = `${margin}px`;
+                    break;
+
+                case WindowPlacement.TopRight:
+                    left = `${right}px`;
+                    top = `${margin}px`;
+                    break;
+
+                case WindowPlacement.Left:
+                    left = `${margin}px`;
+                    top = `${middle}px`;
+                    break;
+
+                case WindowPlacement.Center:
+                    left = `${center}px`;
+                    top = `${middle}px`;
+                    break;
+
+                case WindowPlacement.Right:
+                    left = `${right}px`;
+                    top = `${middle}px`;
+                    break;
+
+                case WindowPlacement.BottomLeft:
+                    left = `${margin}px`;
+                    top = `${bottom}px`;
+                    break;
+
+                case WindowPlacement.Bottom:
+                    left = `${center}px`;
+                    top = `${bottom}px`;
+                    break;
+
+                case WindowPlacement.BottomRight:
+                    left = `${right}px`;
+                    top = `${bottom}px`;
+                    break;
+
+                default:
+                    left = `${center}px`;
+                    top = `${middle}px`;
+                    break;
+            }
+        }
+        else {
+            left = `${this._left}px`;
+            top = `${this._top}px`;
+        }
+
+        state.style.left = left;
+        state.style.top = top;
+
         return this.render(state);
     }
 
@@ -94,6 +181,7 @@ export default class Window extends Component {
 
         let state = new State();
         state.style.display = 'block';
+        state.style.opacity = '1';
         return this.render(state);
     }
 
@@ -105,7 +193,13 @@ export default class Window extends Component {
         this._isOpen = false;
 
         let state = new State();
-        state.style.display = 'none';
-        return this.render(state);
+        state.style.opacity = '0';
+
+        return this.render(state)
+            .then((result) => {
+                let state = new State();
+                state.style.display = 'none';
+                return this.render(state);
+            })
     }
 }
